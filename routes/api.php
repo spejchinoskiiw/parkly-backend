356db62133225ddd94d8c250f428e3ed89180034
stefan.pejchinoski <stefan.pejchinoski@iwconnect.com>
2025-05-10 12:37:18 +0200

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/verify-email', [EmailVerificationController::class, 'verify']);
+
 Route::middleware('auth:sanctum')->group(function () {}
