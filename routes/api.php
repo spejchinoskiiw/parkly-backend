cda079080f89ebf7649c5836174b4d3807831665
stefan.pejchinoski <stefan.pejchinoski@iwconnect.com>
2025-05-10 12:39:54 +0200

    fix api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\EmailVerificationController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
