9bd9592c4f2c7d28aed5367499d4915bda9c7512
stefan.pejchinoski <stefan.pejchinoski@iwconnect.com>
2025-05-10 18:48:21 +0200

    seeder command

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,29 @@
 <?php
 
+use App\Console\Commands\SeedQaEnvironment;
+use Database\Seeders\QaTestingSeeder;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Register QA seeding command as a shortcut
+Artisan::command('qa:seed {--force}', function () {
+    $force = $this->option('force');
+    
+    if ($force || $this->confirm('This will delete all existing data and seed QA test data. Are you sure you want to continue?')) {
+        $this->info('Starting QA environment seeding...');
+        
+        // Run the QA testing seeder
+        $this->call('db:seed', [
+            '--class' => QaTestingSeeder::class,
+            '--force' => true,
+        ]);
+        
+        $this->info('QA environment seeded successfully!');
+    } else {
+        $this->info('Operation cancelled.');
+    }
+})->purpose('Seed the database with QA testing data');
